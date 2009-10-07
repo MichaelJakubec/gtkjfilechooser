bc7c14f38427555a507ae3d5afca2a70df00f830
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-10-07 20:28:18 +0000

    Issue 16:  	 RecentlyUsedManager#readRecentFiles(int): SAX instead of JAXB

--- trunk/GtkJFileChooser/test/eu/kostia/gtkjfilechooser/xbel/RecentlyUsedManagerTest.java
+++ trunk/GtkJFileChooser/test/eu/kostia/gtkjfilechooser/xbel/RecentlyUsedManagerTest.java
@@ -22,29 +22,9 @@ public class RecentlyUsedManagerTest {
 		int count = 1;
 		List<File> fileEntries = m.readRecentFiles(30);
 		Assert.assertEquals(30, fileEntries.size());
-		System.out.println("\nJAXB");
-		for (File file : fileEntries) {			
-			System.out.println(count+") "+file.getName() + "\t" + new Date(file.lastModified()));
-			count++;
-		}
-	}
-	
-	@Test
-	public void testReadRecentFilesSax() throws Exception {
-		File recentlyUsed = new File(System.getProperty("user.home") + File.separator
-				+ ".recently-used.xbel");
-		if (!recentlyUsed.exists()) {
-			recentlyUsed = new File("misc/xbel/recently-used-example.xbel");
-		}
-		RecentlyUsedManager m = new RecentlyUsedManager(recentlyUsed);
-
-
-		int count = 1;
-		List<File> fileEntries = m.readRecentFilesSax(30);
-		Assert.assertEquals(30, fileEntries.size());
 		System.out.println("\nSAX");
 		for (File file : fileEntries) {			
-			System.out.println(count+") "+file.getName() + "\t" + new Date(file.lastModified()));
+			System.out.printf("%2d %s \t %tD\n", count, file.getName() , new Date(file.lastModified()));
 			count++;
 		}
 	}
