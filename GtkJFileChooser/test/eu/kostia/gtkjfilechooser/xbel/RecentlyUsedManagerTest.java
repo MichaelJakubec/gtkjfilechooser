974feb22e08db5ef64c6f1ed93ff5146cbc077a5
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-10-07 21:48:52 +0000

    RecentlyUsedManager create only one time for each instance of GtkFileChooserUI.

--- GtkJFileChooser/test/eu/kostia/gtkjfilechooser/xbel/RecentlyUsedManagerTest.java
+++ GtkJFileChooser/test/eu/kostia/gtkjfilechooser/xbel/RecentlyUsedManagerTest.java
@@ -11,16 +11,24 @@ public class RecentlyUsedManagerTest {
 
 	@Test
 	public void testReadRecentFiles() throws Exception {
-		File recentlyUsed = new File(System.getProperty("user.home") + File.separator
-				+ ".recently-used.xbel");
-		if (!recentlyUsed.exists()) {
-			recentlyUsed = new File("misc/xbel/recently-used-example.xbel");
-		}
-		RecentlyUsedManager m = new RecentlyUsedManager(recentlyUsed);
+		File recentlyUsed = new File(System.getProperty("user.home") + File.separator + ".recently-used.xbel");
 
+		int n = 30;
+		RecentlyUsedManager m = null;
+		if (!recentlyUsed.exists()) {
+			m = new RecentlyUsedManager(n) {
+				@Override
+				protected File getRecentlyUsedFile() {
+					return new File("misc/xbel/recently-used-example.xbel");
+				}
+			};
+			recentlyUsed = new File("misc/xbel/recently-used-example.xbel");
+		} else {
+			m = new RecentlyUsedManager(n);
+		}
 
 		int count = 1;
-		List<File> fileEntries = m.readRecentFiles(30);
+		List<File> fileEntries = m.getRecentFiles();
 		Assert.assertEquals(30, fileEntries.size());
 		System.out.println("\nSAX");
 		for (File file : fileEntries) {			
