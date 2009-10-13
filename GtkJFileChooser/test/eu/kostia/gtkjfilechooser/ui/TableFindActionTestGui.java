c1c8e339f326584f803c887bf41030b2e0cae104
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-10-13 23:16:59 +0000

    Auto-incremental search on JTable

--- GtkJFileChooser/test/eu/kostia/gtkjfilechooser/ui/TableFindActionTestGui.java
+++ GtkJFileChooser/test/eu/kostia/gtkjfilechooser/ui/TableFindActionTestGui.java
@@ -1,10 +1,40 @@
 package eu.kostia.gtkjfilechooser.ui;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
 
-public class TableFindActionTestGui {
-	
-	public static void main(String[] args) {
-		
+import com.sun.java.swing.plaf.gtk.GTKLookAndFeel;
+
+public class TableFindActionTestGui extends JFrame {
+	private JTable jTable;
+
+	public TableFindActionTestGui() {
+		jTable = new JTable();
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+		jTable.setModel(new javax.swing.table.DefaultTableModel(new Object[][] {
+				{ "Costantino", "Cerbo", "Napoli" },
+				{ "Christiane", "Oellig", "Stuttgart" }, 
+				{ "Davide", "Cerbo", "Roma" },
+				{ "Federico", "Cerbo", "Salerno" } 
+		}, 
+		new String[] { "Nome", "Cognome", "Cittá" }));
+
+		new TableFindAction().install(jTable);
+		getContentPane().add(new JScrollPane(jTable));
+	}
+
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(GTKLookAndFeel.class.getName());
+
+		TableFindActionTestGui test = new TableFindActionTestGui();
+		test.pack();
+		test.setVisible(true);
+
+
 	}
 
 }
