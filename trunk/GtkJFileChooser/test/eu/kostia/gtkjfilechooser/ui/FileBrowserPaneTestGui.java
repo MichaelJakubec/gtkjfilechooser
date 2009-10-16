bc702ace237f75020e3094230a5af95740cbc77a
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-10-16 00:03:13 +0000

    New File Browser

--- trunk/GtkJFileChooser/test/eu/kostia/gtkjfilechooser/ui/FileBrowserPaneTestGui.java
+++ trunk/GtkJFileChooser/test/eu/kostia/gtkjfilechooser/ui/FileBrowserPaneTestGui.java
@@ -4,8 +4,15 @@ import static eu.kostia.gtkjfilechooser.ui.JPanelUtil.createPanel;
 import static eu.kostia.gtkjfilechooser.ui.JPanelUtil.show;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 
 import com.sun.java.swing.plaf.gtk.GTKLookAndFeel;
@@ -14,15 +21,51 @@ import eu.kostia.gtkjfilechooser.ui.JPanelUtil.PanelElement;
 
 
 public class FileBrowserPaneTestGui {
+	FileBrowserPane fileBrowser;
+	JTextField locationField;
+
+	public FileBrowserPaneTestGui() {
+		File startDir = new File(System.getProperty("user.home"));
+		fileBrowser = new FileBrowserPane(startDir);
+		locationField = new JTextField(20);
+		JButton goButton = new JButton("Go");
+		goButton.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String location = locationField.getText();
+				fileBrowser.setCurrentDir(new File(location));
+			}
+		});
+
+		fileBrowser.addPropertyChangeListener(propertyChange());
+
+		show(createPanel(
+				new PanelElement(fileBrowser, BorderLayout.CENTER),
+				new PanelElement(createPanel(locationField, goButton), BorderLayout.PAGE_END)
+		));
+	}
+
+	private PropertyChangeListener propertyChange() {
+		return new PropertyChangeListener() {
+
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				String property = evt.getPropertyName();
+				Object value = evt.getNewValue();
+
+				if (JFileChooser.DIRECTORY_CHANGED_PROPERTY.equals(property)) {
+					locationField.setText(((File)value).getAbsolutePath());
+				}
+
+			}
+		};
+	}
+
 	/**
 	 * Main for testing purpose.
 	 */
 	public static void main(String[] args) throws Exception {
 		UIManager.setLookAndFeel(GTKLookAndFeel.class.getName());
-
-		File startDir = new File(System.getProperty("user.home"));
-		FileBrowserPane fileBrowser = new FileBrowserPane(startDir);
-
-		show(createPanel(new PanelElement(fileBrowser, BorderLayout.CENTER)));
+		new FileBrowserPaneTestGui();
 	}
 }
