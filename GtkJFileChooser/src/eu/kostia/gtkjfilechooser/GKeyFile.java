bcd26c1017fdcf587566b873e93efda6ac70e679
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-11-07 01:08:56 +0000

    GKeyFile handle create new .ini files (and parent directories)

--- GtkJFileChooser/src/eu/kostia/gtkjfilechooser/GKeyFile.java
+++ GtkJFileChooser/src/eu/kostia/gtkjfilechooser/GKeyFile.java
@@ -155,6 +155,10 @@ public class GKeyFile {
 
 		if (!gkeyfile.exists()) {
 			//create the file, if not exists
+			if (!gkeyfile.getParentFile().exists()) {
+				//create the parent dirs
+				gkeyfile.getParentFile().mkdirs();
+			}
 			gkeyfile.createNewFile();
 		}
 
