b0a0b22f8522877b52edcbdb6070f108fd8e731b
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-11-26 17:46:00 +0000

    GtkVersion JNI

--- trunk/GtkJFileChooser/src/eu/kostia/gtkjfilechooser/j_gtk_version.c
+++ trunk/GtkJFileChooser/src/eu/kostia/gtkjfilechooser/j_gtk_version.c
@@ -1,8 +1,12 @@
+#include <jni.h>
+#include <stdio.h>
 #include <gtk/gtk.h>
 #include "j_gtk_version.h"
+#include "HelloWorld.h"
+
 
 JNIEXPORT jstring JNICALL Java_eu_kostia_gtkjfilechooser_GtkVersion_check
   (JNIEnv *env, jclass cls, jint required_major, jint required_minor, jint required_micro) {
-  	const char *check_string = gtk_check_version(required_major, required_minor, required_micro);
-  	return (*env)->NewStringUTF(env, check_string);
+  	version gtk_check_version(required_major, required_minor, required_micro);
+  	return (*env)->NewStringUTF(env, version);
 }
