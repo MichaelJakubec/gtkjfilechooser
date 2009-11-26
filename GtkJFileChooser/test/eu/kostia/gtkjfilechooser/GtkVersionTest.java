1f982449e34ea47042c4bb847d2574014c79d091
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-11-26 00:00:23 +0000

    GtkVersion

---
+++ GtkJFileChooser/test/eu/kostia/gtkjfilechooser/GtkVersionTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2009 Costantino Cerbo.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact me at c.cerbo@gmail.com if you need additional information or
+ * have any questions.
+ */
+package eu.kostia.gtkjfilechooser;
+
+import org.junit.Test;
+
+import sun.security.action.GetPropertyAction;
+
+
+/**
+ * @author Costantino Cerbo
+ *
+ */
+public class GtkVersionTest {
+	@Test
+	public void testGetVersion1() {
+		String version = java.security.AccessController.doPrivileged(new GetPropertyAction("swing.gtk.version"));
+		System.out.println("version: " + version);
+		
+	}
+	@Test
+	public void testGetVersion() throws Exception {
+		//TODO
+	}
+}
