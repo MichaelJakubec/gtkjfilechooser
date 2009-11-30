3b00e942d0849365296689dc534d0a4ba905c267
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2009-11-30 21:18:57 +0000

    Issue 44:  	 Get GTK Version

--- GtkJFileChooser/src/eu/kostia/gtkjfilechooser/GtkVersion.java
+++ GtkJFileChooser/src/eu/kostia/gtkjfilechooser/GtkVersion.java
@@ -24,47 +24,113 @@
 package eu.kostia.gtkjfilechooser;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
 
 /**
- * Version Information — Variables and functions to check the GTK+ version
+ * <p>
+ * Version Information for GTK+
+ * </p>
+ * <p>
+ * We get the GNOME version with the ubiquitous command
+ * <tt>gnome-panel --version</tt> and then we retrieve the corresponding GTK+
+ * version from an hard-coded map. Though not the most elegant solution, it has
+ * the advantage to avoid cross-compiling issue like when you use JNI or
+ * external dependencies like JNA (Java Native Access).
+ * </p>
+ * <p>
+ * The shared object containing the version information is
+ * <tt>/usr/lib/libgtk-x11-2.0.so</tt>.
+ * </p>
+ * <p>
+ * Another solution were the interrogate the file
+ * <tt>/usr/lib/pkgconfig/gtk+-2.0.pc</tt> but it isn't always present in a
+ * linux distro.
+ * </p>
  * 
- * @see {@link http
- *      ://library.gnome.org/devel/gtk/unstable/gtk-Feature-Test-Macros.html}
+ * 
+ * 
+ * @see {@link http://library.gnome.org/devel/gtk/unstable/gtk-Feature-Test-Macros.html}
  * @see {@link http://git.gnome.org/cgit/gtk+/tree/gtk/gtkversion.h.in}
  * 
  * @author Costantino Cerbo
  * 
  */
 public class GtkVersion {
-	static public int GTK_MAJOR_VERSION;
-	static public int GTK_MINOR_VERSION;
-	static public int GTK_MICRO_VERSION;
+	static private final Map<String, GtkVersion> GNOME_GTK_MAP = new HashMap<String, GtkVersion>();
 	static {
+		GNOME_GTK_MAP.put("2.28", new GtkVersion(2, 18)); // Sep 2009
+		GNOME_GTK_MAP.put("2.26", new GtkVersion(2, 16)); // Mar 2009
+		GNOME_GTK_MAP.put("2.24", new GtkVersion(2, 14)); // Sep 2008
+		GNOME_GTK_MAP.put("2.22", new GtkVersion(2, 12)); // Mar 2008
+		GNOME_GTK_MAP.put("2.20", new GtkVersion(2, 12)); // Sep 2007
+		GNOME_GTK_MAP.put("2.18", new GtkVersion(2, 12)); // Mar 2007
+		GNOME_GTK_MAP.put("2.16", new GtkVersion(2, 10)); // Sep 2006
+		GNOME_GTK_MAP.put("2.14", new GtkVersion(2, 10)); // Mar 2006
+		GNOME_GTK_MAP.put("2.12", new GtkVersion(2, 6)); // Sep 2005
+		GNOME_GTK_MAP.put("2.10", new GtkVersion(2, 6)); // Mar 2005
+		GNOME_GTK_MAP.put("2.8", new GtkVersion(2, 4)); // Sep 2004
+		GNOME_GTK_MAP.put("2.6", new GtkVersion(2, 4)); // Mar 2004
+	}
+
+	static private final GtkVersion UNKNOWN = new GtkVersion(-1, -1);
+
+	static public GtkVersion current;
+
+	private int major, minor;
+
+	private GtkVersion(int major, int minor) {
+		this.major = major;
+		this.minor = minor;
+	}
+
+	static public GtkVersion getCurrent() {
 		try {
+			return getCurrent0();
+		} catch (Throwable th) {
+			Log.log(Level.WARNING, th, "Cannot detect GTK+ version");
+			return UNKNOWN;
+		}
+	}
+
+	static private GtkVersion getCurrent0() throws IOException {
+		if (current == null) {
 			BufferedReader br = null;
 			try {
-				// pkg-config works on all UNIX-like operating systems
-				String[] cmd = { "pkg-config", "--modversion", "gtk+-2.0" };
+				// gnome-panel is installed on all UNIX-like
+				// operating systems with GNOME
+				String[] cmd = { "gnome-panel", "--version" };
 				Process process = new ProcessBuilder(cmd).start();
-				br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-				String[] arg = br.readLine().split(Pattern.quote("."));
+				br = new BufferedReader(new InputStreamReader(process
+						.getInputStream()));
+				String line = br.readLine();
 
-				GTK_MAJOR_VERSION = Integer.parseInt(arg[0]);
-				GTK_MINOR_VERSION = Integer.parseInt(arg[1]);
-				GTK_MICRO_VERSION = Integer.parseInt(arg[2]);
+				// GNOME major.minor version number (without micro)
+				int start = line.lastIndexOf(' ') + 1;
+				int end = line.lastIndexOf('.');
+				String version = line.substring(start, end);
+
+				GtkVersion result = GNOME_GTK_MAP.get(version);
+				current = result != null ? result : UNKNOWN;
 			} finally {
 				if (br != null) {
 					br.close();
 				}
 			}
-		} catch (Throwable e) {
-			e.printStackTrace();
-			GTK_MAJOR_VERSION = -1;
-			GTK_MINOR_VERSION = -1;
-			GTK_MICRO_VERSION = -1;
 		}
+
+		return current;
+	}
+
+	public int getMajor() {
+		return major;
+	}
+
+	public int getMinor() {
+		return minor;
 	}
 
 	/**
@@ -80,12 +146,22 @@ public class GtkVersion {
 	 *         freed.
 	 */
 	static public Boolean check(int major, int minor, int micro) {
-		if (GTK_MAJOR_VERSION == -1) {
+		if (getCurrent().getMajor() == -1) {
 			return null;
 		}
 
-		return (GTK_MAJOR_VERSION > (major)
-				|| (GTK_MAJOR_VERSION == (major) && GTK_MINOR_VERSION > (minor)) || (GTK_MAJOR_VERSION == (major)
-				&& GTK_MINOR_VERSION == (minor) && GTK_MICRO_VERSION >= (micro)));
+		return (getCurrent().getMajor() > (major)
+				|| (getCurrent().getMajor() == (major) && getCurrent()
+						.getMinor() > (minor)) || (getCurrent().getMajor() == (major) && getCurrent()
+				.getMinor() == (minor)));
+	}
+
+	@Override
+	public String toString() {
+		return major + "." + minor;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(GtkVersion.getCurrent());
 	}
 }
