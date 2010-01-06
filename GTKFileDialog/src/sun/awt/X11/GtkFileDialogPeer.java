205c3379bfb59855a7efcd2287ebf06483dff7d8
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2010-01-06 21:08:55 +0000

    FileDialog: Implementing FileFilter

--- GTKFileDialog/src/sun/awt/X11/GtkFileDialogPeer.java
+++ GTKFileDialog/src/sun/awt/X11/GtkFileDialogPeer.java
@@ -2,37 +2,66 @@ package sun.awt.X11;
 
 import java.awt.FileDialog;
 import java.awt.peer.FileDialogPeer;
+import java.io.File;
 import java.io.FilenameFilter;
 
 public class GtkFileDialogPeer implements FileDialogPeer {
 	private FileDialog target;
-		
+	private FilenameFilter filter;
+
 	public GtkFileDialogPeer(FileDialog target) {
 		super();
 		this.target = target;
 	}
 
-	native public void init(String title);
-	
-	native public void setDirectory(String dir);
+	native void init(String title);
 
-	native public void setFile(String file);
-	
-	native public void setMode(int mode);
-	
-	native protected String run();
+	native void destroy();
 
-	native void setFilenameFilterNative (FilenameFilter filter);
+	native String run();
+
+	native void setMode(int mode);
+
+	native void setDirectoryNative(String dir);
+
+	native void setFileNative(String file);
+
+	native void setFilenameFilterNative(FilenameFilter filter);
+	
+	@Override
+	public void setFile(String file) {
+		setFileNative(file);
+	}
+
+	@Override
+	public void setDirectory(String dir) {
+		setDirectoryNative(dir);
+	}
 
 	@Override
 	public void setFilenameFilter(FilenameFilter filter) {
-		//TODO implement: see gtkfiledialogpeer.c in project classpath
-		//http://csourcesearch.com/java/fid6412A0C71A758438F9E09C45D9AD414C004A43AC.aspx?s=GtkFileDialogPeer.java#L1
-		//http://csourcesearch.com/c/fid91CC5D776375EFBA9028496BF036728F4A1A5A1A.aspx?s=accept#L74
+		this.filter = filter;
+		setFilenameFilterNative(filter);
+		// TODO implement: see gtkfiledialogpeer.c in project classpath
+		// http://csourcesearch.com/java/fid6412A0C71A758438F9E09C45D9AD414C004A43AC.aspx?s=GtkFileDialogPeer.java#L1
+		// http://csourcesearch.com/c/fid91CC5D776375EFBA9028496BF036728F4A1A5A1A.aspx?s=accept#L74
 	}
-	
+
 	public void setVisible(boolean vis) {
 		String filename = run();
 		target.setFile(filename);
+	}
+
+	/*
+	 * This method interacts with the native callback function of the same name.
+	 * The native function will extract the filename from the GtkFileFilterInfo
+	 * object and send it to this method, which will in turn call the filter's
+	 * accept() method and give back the return value.
+	 */
+	boolean filenameFilterCallback(String fullname) {
+		String filename = fullname.substring(fullname.lastIndexOf(File.separator) + 1);
+		String dirname = fullname.substring(0, fullname.lastIndexOf(File.separator));
+
+		return filter.accept(new File(dirname), filename);
 	}
 }
