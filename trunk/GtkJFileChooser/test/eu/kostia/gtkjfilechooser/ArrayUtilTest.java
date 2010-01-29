ee8a1bdc30dab3e9f892fbdfa1caff201009f0c3
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2010-01-29 21:58:42 +0000

    Issue 54: StackOverflowError with OpenJDK 6

---
+++ trunk/GtkJFileChooser/test/eu/kostia/gtkjfilechooser/ArrayUtilTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2009 Costantino Cerbo.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact me at c.cerbo@gmail.com if you need additional information or
+ * have any questions.
+ */
+package eu.kostia.gtkjfilechooser;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+/**
+ * @author Costantino Cerbo
+ *
+ */
+public class ArrayUtilTest {
+
+	@Test
+	public void testAreArrayEqual() {
+		File[] array0 = new File[1];
+		array0[0] = new File("myfile");
+		
+		File[] array1 = new File[1];
+		array1[0] = new File("myfile");
+		
+		System.out.println(System.getProperty("java.vendor") + " " +System.getProperty("java.version"));
+		assertFalse(array0.equals(array1));
+		assertTrue(ArrayUtil.areArrayEqual(array0, array1));
+	}
+
+}
