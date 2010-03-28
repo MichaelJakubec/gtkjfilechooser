14974f145c91c9e57ddbcdc7957efabc1c14a367
c.cerbo <c.cerbo@8e0e4b5e-ab92-11de-a86f-b10fb6d3a35f>
2010-03-28 13:08:40 +0000

    Copyright notice updated to LGPL

--- trunk/GtkJFileChooser/src/eu/kostia/gtkjfilechooser/Strftime.java
+++ trunk/GtkJFileChooser/src/eu/kostia/gtkjfilechooser/Strftime.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Costantino Cerbo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Costantino Cerbo - initial API and implementation
+ ******************************************************************************/
 /*
  * Copyright 1999, 2009 The Apache Software Foundation.
  * 
--- trunk/GtkJFileChooser/test/eu/kostia/gtkjfilechooser/ui/MissingResourceIconTestGui.java
+++ trunk/GtkJFileChooser/test/eu/kostia/gtkjfilechooser/ui/MissingResourceIconTestGui.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Costantino Cerbo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Costantino Cerbo - initial API and implementation
+ ******************************************************************************/
 /*
  * Copyright 2010 Costantino Cerbo.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
